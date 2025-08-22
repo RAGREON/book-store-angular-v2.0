ba110e2efe75ece1f5a615ab8dfb32a31abfd3bd
RAGREON <ragreon809@gmail.com>
2025-08-22 23:17:29 +0545

    (feat) app-route: Add '/' Home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
