97393adaffb1edbe31d390cc9b573e0b91b1e61c
RAGREON <ragreon809@gmail.com>
2025-08-22 23:43:13 +0545

    feat: configure app routes and implement main navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
 import { NgModule } from '@angular/core';
+import { Store } from './store/store';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'store', component: Store }
 ];
 
 @NgModule({
